bf3b56509ea0e6fe78a3a9b99fd17dce45bf0d2c
Mike Schutte <tmikeschutte@gmail.com>
2018-07-08 14:32:00 -0400

    Update leader macros

--- keymap.c
+++ keymap.c
@@ -269,20 +269,22 @@ void matrix_scan_user(void) {
     leading = false;
     leader_end();
 
-    SEQ_ONE_KEY(KC_F) {
-      // Anything you can do in a macro.
-      SEND_STRING("QMK is awesome.");
-    }
-    SEQ_TWO_KEYS(KC_D, KC_D) {
-      SEND_STRING(SS_LCTRL("a")SS_LCTRL("c"));
-    }
-    SEQ_THREE_KEYS(KC_D, KC_D, KC_S) {
-      SEND_STRING("https://start.duckduckgo.com"SS_TAP(X_ENTER));
-    }
-    SEQ_TWO_KEYS(KC_A, KC_S) {
+    /* SEQ_ONE_KEY(KC_F) { */
+    /*   // Anything you can do in a macro. */
+    /*   SEND_STRING("QMK is awesome."); */
+    /* } */
+    /* SEQ_TWO_KEYS(KC_D, KC_D) { */
+    /*   SEND_STRING(SS_LCTRL("a")SS_LCTRL("c")); */
+    /* } */
+    /* SEQ_THREE_KEYS(KC_D, KC_D, KC_S) { */
+    /*   SEND_STRING("https://start.duckduckgo.com"SS_TAP(X_ENTER)); */
+    /* } */
+
+    // SPOTLIGHT SEARCH
+    SEQ_ONE_KEY(KC_S) {
       register_code(KC_LGUI);
-      register_code(KC_S);
-      unregister_code(KC_S);
+      register_code(KC_SPC);
+      unregister_code(KC_SPC);
       unregister_code(KC_LGUI);
     }
   }
